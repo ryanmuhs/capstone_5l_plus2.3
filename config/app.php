65ff0d7eb21bff5000f90daa2439468c980f8865
ryanmuhs <ryansatria@student.telkomuniversity.ac.id>
2025-01-02 23:03:23 +0700

    feat: add Indonesian geolocation

--- config/app.php
+++ config/app.php
@@ -168,6 +168,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+        // Provider Indo
+        AzisHapidin\IndoRegion\IndoRegionServiceProvider::class,
     ])->toArray(),
 
     /*
